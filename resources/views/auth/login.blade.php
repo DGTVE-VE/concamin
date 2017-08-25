49e281241610512e0d6a2bfb5aeefa822db2cd06
Crapaud <renecrapaud@gmail.com>
2017-08-25 17:45:59 -0500

    Registro e inicio de sesión. Cambio de texto en algunas etiquetas.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Inicio de sesión</div>
+                <div class="panel-heading text-center">Inicio de sesión</div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
@@ -42,7 +42,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recuerdame
                                     </label>
                                 </div>
                             </div>
@@ -51,11 +51,11 @@
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Login
+                                    Inicie Sesión
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                                    ¿Olvidó su contraseña?
                                 </a>
                             </div>
                         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -557,12 +557,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Registrar
-                                </button>
-                            </div>
+                        <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                            <button type="submit" class="btn btn-primary">
+                                Registrar
+                            </button>
                         </div>
                     </form>
                 </div>

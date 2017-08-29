39d9b9042adf0e3d9688b417865b040edf808d3c
Crapaud <renecrapaud@gmail.com>
2017-08-29 13:41:41 -0500

    Envio de correo. Modificación en plantilla de correo enviado.

--- resources/views/emails/activacion.blade.php
+++ resources/views/emails/activacion.blade.php
@@ -2,7 +2,7 @@
 <html>
     <body>
         <a href="{{url ('verificaCorreo')}}/{{$correo}}/{{$hash}}">
-            <img src="imagenes/envioCorreo/ligaRegistro.jpg" alt="Activa tu cuenta" class="image-responsive" style="width:650px; height:350px;"/>
+            <img src={{url('imagenes/envioCorreo/ligaRegistro.jpg')}} alt="Activa tu cuenta" class="image-responsive" style="width:650px; height:350px;"/>
          </a>        
     </body>
 </html>

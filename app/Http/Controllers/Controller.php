86ef72cf17c6e830689d9d5baf02b2c7c6f9fda8
Crapaud <renecrapaud@gmail.com>
2017-08-31 17:49:26 -0500

    Activación de correo. Corrección en controlador

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -28,4 +28,17 @@ class Controller extends BaseController
         $states = \App\Escuelas::distinct()->where('municipio', '=', $municipio)->orderBy('centro_educativo')->get(['centro_educativo']);
         return $states;
     }
+    
+    public function activaCorreo(Request $request, $correo, $hash) {
+        $user = \App\User::where('email', '=', $correo)->first();
+
+        if (md5($user->password) == $hash) {
+            $user->activo = 1;
+            $user->save();
+            return Redirect::home()->with('message','¡Bienvenido! Activaste tu cuenta en Cátedra Innovatic 2.0. Ya puedes iniciar sesión');
+//            return view('viewVentana/activacionCorrecta');
+        } else {
+            print 'error';
+        }
+    }
 }
